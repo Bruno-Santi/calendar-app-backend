95c253ab45cfbf23b04939b23331f76cdc5b54ef
Bruno Santimaria <brun0.23@outlook.com>
2023-11-01 18:32:55 -0300

    terminada validacion de registro de usuario, agregada encriptacion de password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,66 @@
 const { request, response } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const createUser = async (req = request, res = response) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email: email });
 
-const createUser = (req = request, res = response) => {
-  const { name, email, password } = req.body;
+    if (user) {
+      return res.status(401).json({
+        ok: false,
+        msg: "El correo ya existe",
+      });
+    }
+    user = new User(req.body);
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+    await user.save();
 
-  res.status(201).json({
-    ok: true,
-    msg: "Registro",
-    name,
-    email,
-    password,
-  });
+    res.status(201).json({
+      ok: true,
+      msg: "Registro exitoso!",
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  res.status(200).json({
-    ok: true,
-    msg: "Login",
-    email,
-    password,
-  });
+  try {
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        msg: "Usuario/Contraseña incorrecto",
+      });
+    }
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario/Contraseña incorrecto",
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      msg: "Login Exitoso!",
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const renewToken = (req, res) => {
