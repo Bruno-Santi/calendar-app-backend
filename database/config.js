a519081d9fde394804580b49db55d8a503986f81
Bruno Santimaria <brun0.23@outlook.com>
2023-11-03 01:54:18 -0300

    env1

--- database/config.js
+++ database/config.js
@@ -6,6 +6,7 @@ const dbConnection = async () => {
     await mongoose.connect(process.env.DB_CNN);
     console.log("DB connection established");
   } catch (error) {
+    console.log(error);
     throw new Error("Error al conectar a la base de datos");
   }
 };
