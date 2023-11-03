c97c26a20176487399e8ae5478cc4d7ed1fa09f0
Bruno Santimaria <brun0.23@outlook.com>
2023-11-03 01:48:42 -0300

    env

--- database/config.js
+++ database/config.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { DB_CNN } = process.env;
+
 const dbConnection = async () => {
   try {
-    await mongoose.connect(DB_CNN);
+    await mongoose.connect(process.env.DB_CNN);
     console.log("DB connection established");
   } catch (error) {
     throw new Error("Error al conectar a la base de datos");
